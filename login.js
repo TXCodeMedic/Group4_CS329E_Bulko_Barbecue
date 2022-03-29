fc1a7302d26e3f5b3cbd19c0e94bd11ac082d7fa
Sawad Kazi <kazisawad@gmail.com>
2022-03-28 23:32:15 -0500

    finishing login and reg page

--- login.js
+++ login.js
@@ -1,12 +1,11 @@
-document.getElementById("register").addEventListener("click", function login() {
-    u = document.getElementById("registration_form").elements[0].value
-    p = document.getElementById("registration_form").elements[1].value
-    r = document.getElementById("registration_form").elements[2].value
+document.getElementById("login").addEventListener("click", function login() {
+    u = document.getElementById("login_form").elements[0].value
+    p = document.getElementById("login_form").elements[1].value
     if (u.length < 6 || u.length > 10 || u.match(/^\W+$/) || u.match(/^\d/)) {
         window.alert('Invalid username or password')
         return (0)
     }
-    if (p != r || p.match(/^\W+$/) || !p.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)")) {
+    if (p.match(/^\W+$/) || !p.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)")) {
         window.alert('Invalid username or password')
         return (0)
     }
